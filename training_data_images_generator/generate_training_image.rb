53c237114efe552324210090f8766f753c91acc7
Ricardo de Cillo <cillo.ricardo@gmail.com>
2012-07-18 10:44:24 -0300

    parametrizando script gerador de imagens de treino

--- training_data_images_generator/generate_training_image.rb
+++ training_data_images_generator/generate_training_image.rb
@@ -7,15 +7,29 @@ Bundler.require
 require 'rexml/document'
 include REXML
 
-file = File.new("input.xml")
+# params should be -xml -i -o -r
+def load_params args
+	params = {}
+	args.each_slice(2) do | key_value |
+		# puts "o #{key_value}"
+		params[key_value[0][1..-1]] = key_value[1]
+	end
+	params
+end
+
+params = load_params(ARGV)
+
+puts params
+
+file = File.new(params["xml"])
 doc = Document.new(file)
 root = doc.root
 
-img = MiniMagick::Image.open("input.png") 
+img = MiniMagick::Image.open(params["i"]) 
 
 img.combine_options do |c| 
 
-root.each_element('//text_region') { |text_region|
+root.each_element("//#{params['r']}") { |text_region|
 	polygon = ""
 	text_region.each_element('coords/point') { |point|
 		polygon = polygon + " #{point.attributes['x']},#{point.attributes['y']}"
@@ -28,5 +42,5 @@ root.each_element('//text_region') { |text_region|
 
 end
 
-img.write  "output.png"
+img.write  params["o"]
 
