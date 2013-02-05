9d19bdb1b4a51f42e6d044d0fc27dafc96c9d0b3
Ricardo de Cillo <cillo.ricardo@gmail.com>
2013-02-05 16:06:08 -0200

    experiments runner

--- scripts/imgset_training_gen.rb
+++ scripts/imgset_training_gen.rb
@@ -4,30 +4,43 @@ require 'fileutils'
 
 $pubs = {
 #  "simple" => [158,171,190,194,195,201,205,232,235,249,263,674,677,680,683,685]
-  "ieee" => [701, 792, 801],
+  # "ieee" => [701, 792, 801]
   "cacm" => [263, 674, 677, 680, 683, 685, 686, 689, 692, 695, 802, 803],
-  "time" => [232, 720, 721, 723, 782, 783, 784, 785, 786],
-  "fortune" => [158, 235, 246, 249, 734, 752, 776],
-  "mixed" => [701,263,801,683,232,723,158,249]
+  # "time" => [232, 720, 721, 723, 782, 783, 784, 785, 786],
+  # "fortune" => [158, 235, 246, 249, 734, 752, 776],
+  # "mixed" => [701,263,801,683,232,723,158,249]
 }
 
 $training_sizes = {
   "one_third" => 0.33,
-  "two_thirds" => 0.66,
+  "half" => 0.5,
+  "two_thirds" => 0.66
 }
 
-$win_sizes = {
-  dense: [[3,3], [4,4], [5,5], [7,7]],
-  sparse: [[5,5], [7,7], [9,9], [11,11]]
-}
+$windows = [
+  {type: :dense, size: [3,3]},
+  {type: :dense, size: [4,4]}
+  # {type: :dense, size: [5,5]},
+  # {type: :dense, size: [6,6]},
+  # {type: :dense, size: [7,7]},
+  # {type: :sparse, size: [3,3]},
+  # {type: :sparse, size: [5,5]},
+  # {type: :sparse, size: [7,7]},
+  # {type: :sparse, size: [9,9]},
+  # {type: :sparse, size: [11,11]}
+]
 
-$regions = {titles: "TextRegion_heading", texts: "TextRegion_paragraph"}
+$regions = ["TextRegion_heading", "TextRegion_paragraph"]
+
 
 $dataset_base_dir = "/mnt/training_set"
-
 $imgsets_base_dir = $dataset_base_dir + "/imgsets"
-
-$win_base_path = "/mnt/manual_tests/windows/"
+$win_base_path = "/mnt/manual_tests/windows"
+$test_results_dir = $dataset_base_dir + "/test_results"
+$ops_base_dir = $dataset_base_dir + "/ops"
+$trios_build = "/home/ubuntu/trioslib-code/bin/bin/trios_build"
+$trios_test = "/home/ubuntu/trioslib-code/bin/bin/trios_test"
+$op_type = "BB"
 
 def write_imgset(f, region, files, file_base)
   f.write("IMGSET  ########################################################\n")
@@ -43,7 +56,7 @@ def write_imgset(f, region, files, file_base)
 end
 
 def gen_test_imgsets(pub)
-  imgsets = {}
+  imgsets = []
   pub_name = pub[0]
   pub_files = pub[1]
   pub_dir = $imgsets_base_dir+"/#{pub_name}"
@@ -62,15 +75,21 @@ def gen_test_imgsets(pub)
     $training_sizes.each { |training_size|
       name = training_size[0]
       tsize = training_size[1]
-      imgset_filename = "imgset_#{region}_#{name}.s"
+      imgset_filename = "test_imgset_#{region}_#{name}.s"
       imgset_absolute_path = pub_dir + "/#{imgset_filename}"
       files = pub_files[([pub_files.size * tsize, 1].max)..-1]
-      imgsets["#{pub_files.size-files.size}"] imgset_absolute_path
+      imgset = {}
+      imgset[:region] = region
+      imgset[:pub_name] = pub_name
+      imgset[:pub_size] = pub_files.size
+      imgset[:size] = files.size
+      imgset[:path] = imgset_absolute_path
       unless File.exists?(imgset_absolute_path)
         f = File.new(imgset_absolute_path, "w+")
         write_imgset(f, region, files, file_base)
         f.close
       end
+      imgsets << imgset
     }
   }
   return imgsets
@@ -78,7 +97,7 @@ end
 
 # training_imgsets = gen_training_imgsets(pub)
 def gen_training_imgsets(pub)
-  imgsets = {}
+  imgsets = []
   pub_name = pub[0]
   pub_files = pub[1]
   pub_dir = $imgsets_base_dir+"/#{pub_name}"
@@ -97,48 +116,88 @@ def gen_training_imgsets(pub)
     $training_sizes.each { |training_size|
       name = training_size[0]
       tsize = training_size[1]
-      imgset_filename = "imgset_#{region}_#{name}.s"
+      imgset_filename = "training_imgset_#{region}_#{name}.s"
       imgset_absolute_path = pub_dir + "/#{imgset_filename}"
       files = pub_files[0..([pub_files.size * tsize, 1].max)]
-      imgsets["#{files.size}"] imgset_absolute_path
+      imgset = {}
+      imgset[:region] = region
+      imgset[:pub_name] = pub_name
+      imgset[:pub_size] = pub_files.size
+      imgset[:size] = files.size
+      imgset[:path] = imgset_absolute_path
       unless File.exists?(imgset_absolute_path)
         f = File.new(imgset_absolute_path, "w+")
         write_imgset(f, region, files, file_base)
         f.close
       end
+      imgsets << imgset
     }
   }
   return imgsets
 end
 
 #  ops = build_ops(training_imgsets)
-def build_ops(pub)
-  pub_name = pub[0]
-  pub_dir = $imgsets_base_dir+"/#{pub_name}"
-  $win_sizes.each { |win_size|  
-    $regions.each { |region| 
-      $training_sizes.each { |training_size|
-        name = training_size[0]
-        imgset_filename = "imgset_#{region}_#{name}.s"
-        imgset_absolute_path = pub_dir + "/#{imgset_filename}"
-        dest_files_base = pub_dir + "/#{win_size}x#{win_size}_#{region}_#{name}"
-        win = $win_base_path + "#{win_size}x#{win_size}.w"
-        system("#{$min_itv_builder} #{win} #{imgset_absolute_path} #{dest_files_base}")
-      }
-    }
-  }
+def build_ops(imgsets)
+  ops = []
+  $windows.each do |win|
+    imgsets do |imgset|
+      # /home/ubuntu/trioslib-code/bin/bin/trios_build BB windows/dense/4x4.w /mnt/training_set/imgsets/simple/imgset_TextRegion_paragraph_one_third.s ops/4x4_one_third_paragraph.op
+      op = {}
+      op[:dir] = "#{$ops_base_dir}/#{imgset[:pub_name]}/#{imgset[:region]}/#{win[:type]}"
+      op[:path] = "#{op[:dir]}/#{win[:size]}.op"
+      op[:imgset] = imgset
+      op[:win] = win
+      system("#{$trios_build} #{$op_type} #{win[:path]} #{imgset[:path]} #{op[:path]}")
+      ops << op
+    end
+  end
+  return ops
+end
+
+def complement_imgset(op, imgsets)
+  imgsets.each do |imgset|
+    return imgset if imgset[:size] + op[:imgset][:size] == imgset[:pub_size]
+  end
+end
+
+# test_results = test_ops(ops, test_imgsets)
+def test_ops(ops, test_imgsets, pub_size)
+  test_results = []
+  ops.each do |op|
+    test_imgset = complement_imgset(op, test_imgsets)
+    result_text = `#{$trios_test} #{op[:path]} #{test_imgset[:path]}`
+    result = {}
+    result[:op] = op
+    result[:test_imgset] = test_imgset
+    result[:text] = result_text
+    result[:path] = $test_results_dir + "/#{result[:imgset][:pub_name]}_#{result[:imgset][:region]}_#{result[:op][:win][:type]}_#{result[:op][:win][:size]}.txt"
+    test_results << result
+  end
+  return test_results
+end
+
+# write_test_results(test_results)
+def write_test_results(results)
+  results.each do |result|
+    f = File.new(result[:path], "w+")
+    f.write(result[:text])
+    f.close
+  end
 end
 
 FileUtils.mkdir($imgsets_base_dir) unless Dir.exists?($imgsets_base_dir)
+FileUtils.mkdir($ops_base_dir) unless Dir.exists?($ops_base_dir)
+FileUtils.mkdir($test_results_dir) unless Dir.exists?($test_results_dir)
+
 $pubs.each { |pub| 
   training_imgsets = gen_training_imgsets(pub)
   test_imgsets = gen_test_imgsets(pub)
   ops = build_ops(training_imgsets)
-  test_results = test_ops(ops, test_imgsets) # test each op (region type X window shape X trainingset size)
+  test_results = test_ops(ops, test_imgsets)
   write_test_results(test_results)
-  stats = fscore_stats(ops, test_imgsets)
-  write_fscore_stats(stats)
-  results = merge_segmentations(ops, test_imgsets)
-  fscore_stats(results)
+  # stats = fscore_stats(ops, test_imgsets)
+  # write_fscore_stats(stats)
+  # results = merge_segmentations(ops, test_imgsets)
+  # fscore_stats(results)
 }
 
